323c8214ff6ec75b1e427328f25e9a6d04d73956
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-19 23:06:38 +0000

    Add 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-printf("\"with proper grammar, but the outcome is a piece of art";)
-return (0);
+int main(void)¨
+{
+	printf("\"with proper grammar, but the outcome is a piece of art";)
+	return (0);
 }
