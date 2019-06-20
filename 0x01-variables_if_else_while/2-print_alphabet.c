c1de9baaaeccb14c77c750d8066083a2e4967a9f
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-20 23:09:51 +0000

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+		char apl;
+
+		for (apl = 'a'; apl <= 'z'; apl++)
+
+		putchar(apl);
+								}
+		putchar('\n');
+
+		return (0);
+}
