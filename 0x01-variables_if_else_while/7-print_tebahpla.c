736a9e9e2312106b56ea39e5112c100e2ad9b045
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-21 23:51:11 +0000

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter='z';
+	while (letter >= 'a')
+	putchar(letter --);
+	}
+	puchar {'/n')
+	return (0);
+}
