0a161d7225cc1ef2193ffa46eccb8201ba77a7e3
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-25 21:34:27 +0000

    Add files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_sign - Entry point
+ *@n: value number
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+
+	int num;
+		if (n > 0)
+		{
+			num = 1;
+			_putchar('+');
+		}
+		if (n < 0)
+		{
+			num = 0;
+			_putchar('0');
+		}
+		if (n == 0)
+		{
+			num = -1;
+			_putchar('-');
+		}
+		return (num);
+}
