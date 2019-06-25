7711bca8b96c357f0147c226cded6688b0866699
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-25 19:59:47 +0000

    Add these

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ *  _islower - Entry point
+ * @c: character lower
  *
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 char lower;
-int i=0;
+int i = 0;
 	lower = 'a';
 
 	while  (lower <= 'z')
 	{
-		if(lower == c)
-			i=1;
+		if (lower == c)
+			i = 1;
 		lower++;
 	}
 	return (i);
