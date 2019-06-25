0b5e9a2c64637e93bd0ef90a825b813fe3d62fb7
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-25 18:07:53 +0000

    Add 2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+	while (i != 10)
+		{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+		}
+	_putchar('\n');
+}
