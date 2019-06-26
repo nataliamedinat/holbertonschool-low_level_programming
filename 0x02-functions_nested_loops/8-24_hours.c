0d89ad4c6cc0a38bf70b23afdf4ed4d0dec57e04
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-26 20:13:55 +0000

    Add files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,25 @@
 #include "holberton.h"
 /**
- * main - Entry point
- *
+ * jack_bauer - Entry point
+ *@h: value int
+ *@m: value int
  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
+{
+int h, m;
 
+	for (h = 0; h <= 23; h++)
+	{
+	for (m = 0; m <= 59; m++)
+	{
+	_putchar(h / 10 + '0');
+	_putchar(h % 10 + '0');
+	_putchar(':');
+	_putchar(m / 10 + '0');
+	_putchar(m % 10 + '0');
+	_putchar('\n');
+	}
+	}
+}
 
