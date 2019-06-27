5c6db4b5c04edc2ab1c05718e349b5216dd2c2e4
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-27 23:09:04 +0000

    add 6 files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_line - Entry point
+ *@n: numer
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+
+int i;
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
