7352e5bb9df320c1a48191357471fca00cfc3e70
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-27 19:24:11 +0000

    Add files 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ *@c: character received
+ * Return: 0 (Succes)
+ */
+int _isdigit(int c)
+{
+	if (c <= '0' && c <= 9)
+		return (1);
+	else
+		return (0);
+}
+
