c73825bd1d29eb0bfb7653933f57c6473998ab69
nataliamedinat <nataliamedinabotero@gmail.com>
2019-06-27 17:03:51 +0000

    Add 2 files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include "holberton.h"
+/**
+ * mul - Entry point
+ *@a: integer value
+ *@b:integer value
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_H
 int _isupper(int c);
 
+int mul(int a, int b);
 #endif
