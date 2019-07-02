6946315e9cd42a7dc0a6ae35f802d9b92d5b55ac
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-02 20:13:53 +0000

    Add files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts - check the code for Holberton School students.
+ *@str: pointer
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar (str[i]);
+	}
+		_putchar('\n');
+}
