a54bf10f43656f88c581a492475af1ca3686f25d
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-05 04:07:16 +0000

    changes all lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * string_toupper - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *)
+{
+int pi; /**posicion inicial*/
+
+	for (pi = 0; min[pi] != '\0'; pi++)
+	/**Posicion inicial que sea diferente a null y se sume*/
+	{
+		if (min[pi] >= 97 && min[pi] <= 122)
+	/**Si posicion inicial es mayor o igual a 97 y menor o igual a 122 so*/
+		{
+			min[pi] = min[pi] - 32;
+	/**Posicion inicial es igual a si misma diferencia de 32*/
+		}
+	}
+	return (min);/**Retorna el valor*/
+}
