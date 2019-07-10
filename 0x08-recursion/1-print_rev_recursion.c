2542951f49bd6b1dc4ffec674aa3597cba68317b
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-10 19:21:03 +0000

    Prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ *@s: pointer
+ *
+ * Return: none
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+
+}
