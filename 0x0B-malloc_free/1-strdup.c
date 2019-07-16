7631041455b0febb58ba82417cd0d07539f0ab18
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-16 23:01:06 +0000

    add file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - duplicate string
+ *@str: value
+ *@cpy: copy
+ * Return: null.
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+
+		i++;
+		cpy= malloc(i* sizeof(char));
+
+		if (cpy == NULL)
+		{
+		return (NULL);
+		}
+
+	for (j = 0; j < i; i++)
+	{
+		cpy[j] = cpy[j];
+	}
+		return (cpy);
+}
