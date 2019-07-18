aa481914c779a668d5614999564fa48295acdf7f
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-18 21:58:21 +0000

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= _strlen(s2))
 		n = _strlen(s2);
 
-	p = malloc(sizeof(char) *(_strlen(s1) + n + 1));
+	p = malloc(sizeof(char) * (_strlen(s1) + n + 1));
 	if (p == NULL)
 	{
 	return (NULL);
