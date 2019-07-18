79f8045e3b2e48ecfe1aae4781a9403a69ebb2c5
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-18 06:15:25 +0000

    file

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+	int j, i, acum = 0, cont = 0;
+	char *p;
+
+	
+
+
+
+
+}
+
