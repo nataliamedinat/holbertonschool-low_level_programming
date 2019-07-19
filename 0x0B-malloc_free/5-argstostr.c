d8bc6cb6a7484a6d6587096907ca5c3689036869
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-19 04:35:43 +0000

    fixed

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,18 +1,51 @@
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
+ * _strlen - Calcualte the lenght of an array
+ * @s: The array to be calculated
  *
- * Return: Always 0.
+ * Return: On success a pointer.
+ * On error, NULL is returned.
+ */
+	int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * argstostr - concatenates all arguments in the program
+ *@ac: argumnet counter
+ *@av: argument value
+ * Return: pointer
  */
 char *argstostr(int ac, char **av)
 {
-	int j, i, acum = 0, cont = 0;
-	char *p;
-
 	
+	char *p;
+	int i, j, sum = 0, cont = 0;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		sum += _strlen(*(av + i));
 
+	p = malloc(sizeof(char) * (sum + ac + 1));
+	if (p == NULL)
+		return (NULL);
 
-
+	for (j = 0; j < ac; j++)
+	{
+		for (i = 0; i < _strlen(*(av + j)); i++)
+		{
+			p[cont] = *(*(av + j) + i);
+			cont++;
+		}
+		p[cont] = '\n';
+		cont++;
+	}
+	p[cont] = '\0';
+	return (p);
 }
-
