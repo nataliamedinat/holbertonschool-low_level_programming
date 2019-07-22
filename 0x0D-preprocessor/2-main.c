3268acc33bba722e7fc76739a1e7317c9e42ec51
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-22 21:30:10 +0000

    fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main ()
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	printf ("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
