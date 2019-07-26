538d34e190a46a1127a8f7b70978b647ec5515de
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-26 22:27:28 +0000

    created file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings
+ *@separator: string
+ *@n: number of strings
+ *
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	
+
+
+
+
+
+}
