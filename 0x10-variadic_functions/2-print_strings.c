2c0d0d8165bd672a25825406e5a787f08a85e4bc
nataliamedinat <nataliamedinabotero@gmail.com>
2019-07-27 19:24:03 +0000

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", p);
 			else
 				printf("(nil)");
-			if (separator != NULL && i != (n - 1)
+			if (separator != NULL && i != (n - 1))
 				printf("%s", separator);
 			i++;
 		}
