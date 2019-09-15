18437a1862287d10401b0bf39f528aeff72e0641
nataliamedinat <nataliamedinabotero@gmail.com>
2019-09-15 03:12:57 +0000

    node at the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,42 @@
 #include "lists.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * add_dnodeint_end - adds a new node at the end of a lsit.
+ *@head: Pointer to the list
+ *@n: new element value.
+ * Return: Address of the new element, or null if it failed
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 	dlistint_t *tmp;
-	
+
 	if (head == NULL) /* If the list is empty*/
-		return(NULL);
-	
-	newnode = malloc(sizeof(dlistint_t))
+		return (NULL);
+
+	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL) /* If malloc fails*/
-		return(NULL)
+		return (NULL);
 
 	newnode->n = n; /*Assigning value to the new node*/
 	newnode->next = NULL; /* Last node*/
 
-	if (*head == NULL) 
+	if (*head == NULL)
+	{
 		newnode->prev = NULL;
 		(*head) = newnode; /* New node becomes the head*/
-		tmp = head->next; /* Stores the head*/
-		tmp->next = newnode; 
-		newnode->prev =tmp;
-	
-	
+		return (newnode);
+	}
+
+		tmp = *head; /* Stores the head*/
 
 
+	while (tmp->next != NULL) /* Goes thru the list*/
+	{
+		tmp = tmp->next;
+	}
+		tmp->next = newnode;
+		newnode->prev = tmp;
 
-
-
-
+	return (newnode);
 
 }
