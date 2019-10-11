3524f5085fb4ecd649772a7fcfb92eb410ce8e78
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-11 18:56:52 +0000

    creates hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,10 +5,21 @@
  * Return: Pointer to the newly created hash table, on error, return NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL; /* New table created*/
 
-	hash_table_t *new_table /* New table created*/
+	if (size == 0)
+		return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t)); /* Malloc for the new table*/
 	if (new_table == NULL) /* If malloc fails*/
 		return (NULL);
 
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	/* Create the array and reserve de space*/
+	if (new_table == NULL)
+		return (NULL);
+
+	return (new_table);
+}
