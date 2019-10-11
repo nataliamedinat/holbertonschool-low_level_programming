2bdf1ae27f75270f69294a779d90bb161dcffad8
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-11 20:09:08 +0000

    Function that gives you the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -25,7 +25,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (size == 0)
-		return (NULL);
+		return (0);
 
 	return (hash_djb2(key) % size);
 
