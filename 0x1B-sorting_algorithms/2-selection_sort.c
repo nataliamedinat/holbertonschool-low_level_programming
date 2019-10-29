911ca8fa3ca4d6b749f364e54f56d8ef5007273d
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-29 00:00:35 +0000

    Selection sort

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * selection_sort - Sorts an array of integers in ascending order
  *@array: Pointer to the array
@@ -9,24 +10,36 @@
 void selection_sort(int *array, size_t size)
 {
 
-	unsigned int i, j, min, tmp;
+	unsigned int i, j, k, flag, min, tmp, flag_chg = 0;
 
 	if (size < 2)
 	return;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = i + 1; j < size; j++)
+		min = i;
+		flag_chg = 0;
+		flag = 0;
+	for (j = i + 1; j < size; j++)
 	{
-			if (array[j] < array[min])
+		if (array[j] < array[min])
+		{
+			flag_chg = 1;
 			min = j;
+		}
 	}
 
 	tmp = array[i];
 	array[i] = array[min];
 	array[min] = tmp;
-	print_array(array, size);
+
+	for (k = 0; k < size; k++)
+	{
+		if (array[k] > array[k + 1])
+		flag = 1;
 	}
+		if (flag == 1 && flag_chg == 1)
+		print_array(array, size);
+	}
+	print_array(array, size);
 }
-
-
