1b949779f716f8af2f0170eae7d98cb520840059
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-30 00:38:56 +0000

    nothing hehe

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list
+ *@list: Double pointer to list
+ *
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i = list; /* For move*/
+	int idx = list;
+	int tmp;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	while (list != NULL)
+	{
+		if ( i == idx)
+			j = j->next;
+
+
+
+
+
+
+	}
+
+
+
+
+}
