b1e1323a459b80f499333ce15db21922ab4d4eb5
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-30 17:14:42 +0000

    Added prototypes of the function of the quick sort

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -21,6 +21,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int lower, int upper);
+void swap(int *array, size_t size, int *i, int *j);
+void fast_sort(int *array, size_t size, int lower, int upper);
+void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 #endif
