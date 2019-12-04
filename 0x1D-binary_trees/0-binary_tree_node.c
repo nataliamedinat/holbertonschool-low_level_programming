fccf39b7b8041e75ea7316aa59475c1bd6662ce3
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-04 19:41:22 +0000

    Creates a binary tree node

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode; /* New node created */
 
-	newnode =malloc(sizeof(binary_tree_t));
+	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 		return (NULL);
 
