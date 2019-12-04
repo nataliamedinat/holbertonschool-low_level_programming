00ee7de30c0fc78768782e1904c0c3d748db27d8
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-04 19:37:13 +0000

    Creates a binary tree node

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_node: Creates a binary tree node.
+ *binary_tree_node- Creates a binary tree node.
  *@parent: Pointer  to the parent node of the node to create
  *@value: Value to put in the new node
  * Return: Return a pointer to the new node, NUll on failure
@@ -9,20 +9,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode; /* New node created */
 
-	newnode =malloc(sizeof(binary_tree_t))
+	newnode =malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
-		return NULL;
+		return (NULL);
 
+	newnode->n = value; /* Put the value on the new node*/
+	newnode->parent = parent;
 
-
-
-
-
-
-
-
-
-
-
-
+	return (newnode);
 }
