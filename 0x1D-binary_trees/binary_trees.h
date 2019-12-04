7e89effcf485e88290e66abc14803fa348b92add
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-04 19:38:04 +0000

    Header file with structures and prototypes

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -1,5 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,3 +22,5 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+#endif
