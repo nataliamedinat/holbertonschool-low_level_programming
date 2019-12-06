55801df17ee36f8fc6301e037a436ab7fcb82c86
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-06 22:59:58 +0000

    Finds the uncle of a node

---
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling
+ * Return: Siblings
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ *@node: Pointer to the node to find the uncle
+ * Return: Pointer to the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent)
+		return (binary_tree_sibling(node->parent));
+	else
+		return (NULL);
+}
