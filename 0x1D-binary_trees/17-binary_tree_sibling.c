08012063ff4f672ac6e177c5ef4df1ab9a2ff05c
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-06 22:28:07 +0000

    Finds the sibling of a node

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -6,7 +6,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL)
+	if (!node->parent || !node )
 		return (NULL);
 
 	if (node->parent->left == node)
